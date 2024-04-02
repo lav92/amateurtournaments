779605f2d83f463de226470441caffbae105678c
avl92linux <xxkukucxx@gmail.com>
2024-04-03 00:15:30 +0300

    создание модели team, связь ее с моделью user, начало работы с фронтом

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, Request
+from typing import Optional
+
+from fastapi import FastAPI, Request, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from app.users.router import router as user_router
 from app.teams.router import router as team_router
-from app.frontend.router import router as frontend_router
 
 app = FastAPI(
     title="Hello World",
@@ -16,9 +17,24 @@ app.mount("/static", StaticFiles(directory="app/templates/static", html=True), n
 
 app.include_router(user_router)
 app.include_router(team_router)
-app.include_router(frontend_router)
 
 
 @app.get("/")
 async def root(request: Request):
-    return templates.TemplateResponse(name='index.html', context={"request": request})
+    return templates.TemplateResponse(name='home.html', context={"request": request})
+
+
+@app.get("/register")
+async def register(request: Request):
+    return templates.TemplateResponse(name='registration.html', context={"request": request})
+
+
+@app.post('/join')
+async def join(
+        email: str = Form(),
+        password: str = Form(),
+        first_name: str = Form(),
+        last_name: str = Form(),
+        nickname: str = Form(),
+):
+    print(email, password, first_name, last_name, nickname)
