ee26c35674a6056c46e5158b690b6e82f0c10d41
avl92linux <xxkukucxx@gmail.com>
2024-04-28 13:17:58 +0300

    API

--- test.py
+++ test.py
@@ -1,24 +1,46 @@
-import asyncio
-from app.teams.models import Team
-from app.teams.dao import TeamDAO
+import requests
 
-from app.users.models import User
-from app.users.dao import UsersDAO
+"""
+    open dota api
+"""
+# url_match = "https://api.opendota.com/api/matches/7707960268"
+#
+# url_request_match = "https://api.opendota.com/api/request/7707960268"
+# url_job = "https://api.opendota.com/api/request/196559881"
+#
+# parsed = "https://api.opendota.com/api/parsedMatches"
+# # response = requests.get(url_match)
+# # response_req = requests.post(url_request_match) #{"job":{"jobId":196559881}}
+# response = requests.get(url_job)
+#
+# # print(response_req.text)
+# # print(response.text)
+# print(response, response.text, response.content, sep='\n')
+#
+# response_parse = requests.get(parsed)
+#
+# print(response_parse.text)
 
-from app.database import async_session_maker
-from sqlalchemy import select
+"""
+    stratz api
+    token - eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJTdWJqZWN0IjoiYTgzM2QwOTEtNzFlZS00MWIzLWJiZjUtMDllYTAxZjYyNTA1IiwiU3RlYW1JZCI6IjEyMzMwNDUwNjQiLCJuYmYiOjE3MTQyNDI5NzcsImV4cCI6MTc0NTc3ODk3NywiaWF0IjoxNzE0MjQyOTc3LCJpc3MiOiJodHRwczovL2FwaS5zdHJhdHouY29tIn0.WAk48WeRVjCcO1r3cFVxwOKH9u4PwhN3ix64a-72uS4
+"""
 
+BASE_URL = "https://api.stratz.com"
 
-async def test():
-    user: User = await UsersDAO.find_or_none(id=10)
-    team: Team = await TeamDAO.find_or_none(id=1)
-    print(user.email, team.title)
+ABILITY_URL = f"{BASE_URL}/api/v1/Ability"
+GAME_VERSION_URL = f"{BASE_URL}/api/v1/GameVersion"
+HERO_URL = f"{BASE_URL}/api/v1/Hero"
+ITEM_URL = f"{BASE_URL}/api/v1/Item"
+MATCH_URL = f"{BASE_URL}/api/v1/match/7707584671"
+LANG_URL = f"{BASE_URL}/api/v1/Language"
+PLAYER_URL = f"{BASE_URL}/api/v1/Player"
 
-    async with async_session_maker() as session:
-        team.members = user
-        query = select(Team).filter_by(id=1)
-         
+HEADERS = {
+    "Authorization": "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJTdWJqZWN0IjoiYTgzM2QwOTEtNzFlZS00MWIzLWJiZjUtMDllYTAxZjYyNTA1IiwiU3RlYW1JZCI6IjEyMzMwNDUwNjQiLCJuYmYiOjE3MTQyNDI5NzcsImV4cCI6MTc0NTc3ODk3NywiaWF0IjoxNzE0MjQyOTc3LCJpc3MiOiJodHRwczovL2FwaS5zdHJhdHouY29tIn0.WAk48WeRVjCcO1r3cFVxwOKH9u4PwhN3ix64a-72uS4"}
 
-    print(type(async_session_maker))
+my_steam_id = "1233045064"
 
-asyncio.run(test())
+response = requests.get(MATCH_URL, headers=HEADERS)
+
+print(response, response.text, response.content, '\n')
